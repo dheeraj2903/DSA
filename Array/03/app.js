ecee986991652f5088447b3c8540a205a05f7f39
dheeraj <dheerajverma2903@gmail.com>
2026-09-14 23:23:04 +0530

    Rotations-question-solved

--- Array/03/app.js
+++ Array/03/app.js
@@ -56,4 +56,50 @@ while(i<j){
 }
 console.log(arr)
 }
-reverseNum()
+//Left Rotation only 1 position Shift
+
+function leftRotation(){
+    let arr = [1,2,3,4,5,6];
+
+    let copy = arr[0];
+
+    for(let i=0; i<arr.length - 1; i++){
+        arr[i] = arr[i+1];
+    }
+    arr[arr.length-1] = copy
+
+    console.log(arr)
+}
+
+//Right Rotation only 1 position shift
+
+function rightRotation(){
+    let arr = [1,2,3,4,5,6];
+
+    let copy = arr[arr.length - 1];
+
+    for(let i=arr.length-1; i>0; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = copy;
+
+    console.log(arr)
+}
+
+//Left Rotation only k position BruteForce
+
+function kRotation(){
+    let arr = [1,2,3,4,5,6];
+    let k=3;
+
+    for(let j=0; j<=k; j++){
+        let copy = arr[0];
+
+        for(let i=0; i<arr.length-1; i++){
+            arr[i] = arr[i+1]
+        }
+        arr[arr.length-1] = copy
+    }
+    console.log(arr)
+}
+kRotation()
