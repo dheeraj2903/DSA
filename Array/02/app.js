91fcd8b1b9704186d3a48b89f86f5c9a4488b7f8
dheeraj <dheerajverma2903@gmail.com>
2026-09-16 23:41:18 +0530

    blockswap & Linear search added

---
+++ Array/02/app.js
@@ -0,0 +1,23 @@
+//Binary Search Algorithm
+
+
+function binarySearch() {
+
+    let arr = [10, 23, 39, 47, 57, 68, 74, 89, 101, 125]
+
+    if(search(arr, 55) === -1) console.log('Element not Found');
+    else console.log("Element found");
+    
+    function search(arr, target){
+        let start = 0, end = arr.length - 1;
+
+        while(start<=end){
+            let mid = Math.floor(start + (end - start)/2);
+            
+            if(arr[mid] === target) return mid;
+            else if(arr[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+        return -1
+    }
+}
