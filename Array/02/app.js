3d4c775e25e52c6003dbd43627867a88db3e3a24
dheeraj <dheerajverma2903@gmail.com>
2026-09-17 23:16:27 +0530

    bubble-sort-solved

--- Array/02/app.js
+++ Array/02/app.js
@@ -21,3 +21,26 @@ function binarySearch() {
         return -1
     }
 }
+
+
+//Bubble Sort
+
+function bubbleSort(){
+
+    let arr = [1, 3, 4, 2, 9, 10];
+
+    let n = arr.length;
+
+    for(let i = 0; i<n-1; i++){
+        for(let j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp
+            }
+        }
+    }
+    console.log(arr);
+    
+}
+bubbleSort()
