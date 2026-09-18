23d8db1db13826eb8677067ba9aba9b903dff14c
dheeraj <dheerajverma2903@gmail.com>
2026-09-18 23:41:03 +0530

    selection-sort-solved

--- Array/02/app.js
+++ Array/02/app.js
@@ -43,4 +43,30 @@ function bubbleSort(){
     console.log(arr);
     
 }
-bubbleSort()
+
+
+// Selection Sort Algorithm
+
+function selectionSort(){
+    
+    let arr=[100, 3, 5, 1, 14];
+
+    for(let i=0; i<arr.length-1; i++){
+
+        let small = i;
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[small] > arr[j]){
+                small=j
+            }
+        }
+
+        if(i != small){
+            let temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+        }
+    }
+    console.log(arr);
+    
+}
+selectionSort()
