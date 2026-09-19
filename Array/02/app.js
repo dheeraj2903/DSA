5e616fcc7b3d803ef78a7dabb235a33759f3363f
dheeraj <dheerajverma2903@gmail.com>
2026-09-19 23:32:12 +0530

    insertionSort-solved

--- Array/02/app.js
+++ Array/02/app.js
@@ -79,9 +79,9 @@ function insertionSort(){
     for(let i=1; i<arr.length; i++){
         
         let key = arr[i];
-        let j = i - 1;
+        let j = i-1;
 
-        while(j>=0 && arr[i]>key){
+        while(j>=0 && arr[j]>key){
             arr[j+1] = arr[j]
             j--
         }
