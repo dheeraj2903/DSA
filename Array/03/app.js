c7a138b56f9631342db77966edaf4f13f7f678b8
dheeraj <dheerajverma2903@gmail.com>
2026-09-23 22:44:54 +0530

    rotateWIthTranspose

--- Array/03/app.js
+++ Array/03/app.js
@@ -80,98 +80,130 @@ function dynamic3D() {
 
 // 2D Diagonal sum
 
-function diagonalSum(){
-    let arr = [
+function diagonalSum() {
+  let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  let leftSum = 0,
+    rightSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (i == j) leftSum += arr[i][j];
+      if (i + j == arr.length - 1) rightSum += arr[i][j];
+    }
+  }
+  console.log("Left sum:", leftSum);
+  console.log("LefRight sum:", rightSum);
+  console.log("Both sum:", leftSum + rightSum);
+}
+
+//Diagonal repeat element handled
+
+function diagonalLeftRightSum() {
+  let arr = [
+    [1, 2, 3],
+    [4, 10, 6],
+    [7, 8, 9],
+  ];
+
+  let leftSum = 0,
+    rightSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (i == j) leftSum += arr[i][j];
+      if (i + j == arr.length - 1) rightSum += arr[i][j];
+    }
+  }
+
+  if (arr.length % 2 == 1) {
+    let mid = Math.floor(arr.length / 2);
+
+    return leftSum + rightSum - arr[mid][mid];
+  }
+
+  return leftSum + rightSum;
+}
+
+// Transpose matrix by method
+function trnasposeMatrix() {
+  let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+  ];
+
+  let ans = Array.from({ length: arr[0].length }, () => Array(arr.length));
+
+  for (let i = 0; i < ans.length; i++) {
+    for (let j = 0; j < ans[i].length; j++) {
+      ans[i][j] = arr[j][i];
+    }
+  }
+
+  return ans;
+}
+
+// Transpose matrix by normal waa\y
+function trnasposeMat() {
+  let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+  ];
+
+  let row = arr.length;
+  let col = arr[0].length;
+  let ans = new Array(col);
+
+  for (let i = 0; i < ans.length; i++) {
+    ans[i] = new Array(row);
+  }
+
+  for (let i = 0; i < ans.length; i++) {
+    for (let j = 0; j < ans[i].length; j++) {
+      ans[i][j] = arr[j][i];
+    }
+  }
+  return ans;
+}
+
+// Rotate Image with transpose LeetCode
+
+function rotateImage(){
+  let matrix = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
 
 
-let leftSum = 0 , rightSum = 0;
-
-for(let i=0; i<arr.length; i++){
-    for(let j=0; j<arr[i].length; j++){
-        if(i==j) leftSum += arr[i][j];
-        if(i+j == arr.length-1) rightSum += arr[i][j]
-    }
-}
-console.log('Left sum:', leftSum);
-console.log('LefRight sum:', rightSum);
-console.log( 'Both sum:' ,leftSum+rightSum);
-
+for(let i=0; i<matrix.length; i++){
+  for(let j=i; j<matrix[i].length; j++){
+    let temp = matrix[i][j];
+    matrix[i][j] = matrix[j][i];
+    matrix[j][i] = temp
+  }
 }
 
-//Diagonal repeat element handled
+for(let i=0; i<matrix.length; i++){
+  let k=0, j=matrix[i].length - 1;
 
-function diagonalLeftRightSum(){
-    let arr = [
-  [1, 2, 3],
-  [4, 10, 6],
-  [7, 8, 9],
-];
+  while(k<j){
+    let temp = matrix[i][k];
+    matrix[i][k] = matrix[i][j];
+    matrix[i][j] = temp
 
+     k++;
+  j--;
+  }
 
-let leftSum = 0 , rightSum = 0;
-
-for(let i=0; i<arr.length; i++){
-    for(let j=0; j<arr[i].length; j++){
-        if(i == j) leftSum += arr[i][j];
-        if(i+j == arr.length-1) rightSum+= arr[i][j]
-    }
 }
 
-if(arr.length % 2 == 1){
-    let mid = Math.floor(arr.length / 2)
+console.log(matrix);
 
-    return leftSum + rightSum - arr[mid][mid]
 }
 
-return leftSum + rightSum
-}
-
-
-// Transpose matrix by method
-function trnasposeMatrix(){
-    
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6]
-];
-
-let ans = Array.from({length:arr[0].length}, ()=> Array(arr.length));
-
-for(let i=0; i<ans.length; i++){
-    for(let j=0; j<ans[i].length; j++){
-        ans[i][j] = arr[j][i]
-    }
-}
-
-return ans
-}
-
-// Transpose matrix by normal waa\y
-function trnasposeMat(){
-    let arr = [
-  [1, 2, 3],
-  [4, 5, 6]
-];
-
-let row = arr.length;
-let col = arr[0].length;
-let ans = new Array(col);
-
-for(let i=0; i<ans.length; i++){
-    ans[i] = new Array(row)
-}
-
-for(let i=0; i<ans.length; i++){
-    for(let j=0; j<ans[i].length; j++){
-        ans[i][j] = arr[j][i]
-    }
-}
-
-return ans
-}
-
-console.log(trnasposeMat());
+rotateImage()
